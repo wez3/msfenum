b9d982fe3cee724be34b8ffec0dc83e0a78bccb3
Ag0s <7050696+Ag0s@users.noreply.github.com>
2018-06-18 16:09:54 +0200

    getSuccessfull summary updated
    Added headers per module when printing summary. Also added PEP287 compliant function comments.

--- msfenum.py
+++ msfenum.py
@@ -2,20 +2,25 @@
 
 import logging, time, json, argparse, fileinput
 from os import listdir, system, path, makedirs
-from sys import exit
+from sys import exit, stdout
 
 modulesfolder = "modules"
 
-# Load the config
 def loadConfig():
+	"""
+	Loads the configuration files
+	"""
 	try:
 		with open('config') as f:
 			return json.load(f)
 	except:
 		logging.error("Failed to load config")
 
-# Valid the config
+
 def validateModuleConfig(modules, modulesconfig):
+	"""
+	Validates the module cofig. 
+	"""
 	missing = []
 	for module in modules:
 		modulename = module.split("/")[-1]
@@ -24,8 +29,11 @@ def validateModuleConfig(modules, modulesconfig):
 	if missing:
 		logging.warning("missing the following module(s): " + "".path.join(missing))
 
-# Generate a RC file
+
 def generateRcs(targets, threads, currentTime, config):
+	"""
+	Compiles all module configurations into one RC file. 
+	"""
 	postmodule = "spool off\n\n"
 	premodule = "spool logs/"
 	modules = config.get('modules')
@@ -51,20 +59,37 @@ def generateRcs(targets, threads, currentTime, config):
 	rcoutput.write(rcfile)
 	rcoutput.close()
 
-# Run msfconsole
+
+
 def runRcs(currentTime):
+	"""
+	Runs metasploit commands and prints output
+	"""
 	logging.info('--- Starting msfconsole ---')
 	system('msfconsole -r logs/' + currentTime + '/file.rc')
 	logging.info('--- Msfconsole done ---')
 
-# Get a summary of discovered output
+
 def getSuccessful(currentTime):
+	"""
+	Prints all [+] entries in the log in context. 
+	"""
 	logging.info('--- Summary of discovered results ---')
-	logging.info(system('grep [+] logs/' + currentTime + '/*.log'))
+	for f in listdir('logs/'+ currentTime):
+		if f.endswith(".log"):
+			logging.info('- Module: ' + f.rsplit('.', 1)[0])
+			result = system('grep [+] logs/' + currentTime + '/' + f)
+			if result == 256: # No results end in a 256 print
+				logging.info("[-] No results")
+			else:
+				logging.info(result)
 	logging.info('--- Msfenum done ---')
 
-# We need some ascii
+
 def ascii():
+	"""
+	We need some ASCII
+	"""
 	print(r"""
 	             . --- .
 	           /        \
@@ -80,8 +105,9 @@ def ascii():
 	       ___\           /___
 	       ~;_  >- . . -<  _i~
 	          `'         `'
-	   By: @wez3forsec, @rikvduijn
-	""")
+	      By: @wez3forsec, @rikvduijn
+	      """)
+
 
 if __name__ == '__main__':
 	# Define variables
@@ -89,13 +115,13 @@ if __name__ == '__main__':
 	targets = []
 	currentTime = int(time.time())
 	currentDir = 'logs/' + str(currentTime)
-	threads = None
+	threads = 10 # default set to 10
 
 	# Define logger settings
 	logging.basicConfig(filename=logfile, level=logging.INFO)
 	logging.getLogger().addHandler(logging.StreamHandler())
-	logging.info('--- Starting msfenum ---')
 	ascii()
+	logging.info('--- Starting msfenum ---')
 
 	# Create current run directory
 	logging.info('[*] Saving msfenum logs in: ' + currentDir)
